8cf0d1a03b8db5999a5ffb084bb02fa2ee38e882
Pawan <respropawan@gmail.com>
2026-01-13 16:37:20 +0530

    Changed CSS properties

--- src/main/java/com/excelr/BloggingAppApplication.java
+++ src/main/java/com/excelr/BloggingAppApplication.java
@@ -8,8 +8,6 @@ public class BloggingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingAppApplication.class, args);
-		
-		System.out.println("welcome");
 	}
 
 }
